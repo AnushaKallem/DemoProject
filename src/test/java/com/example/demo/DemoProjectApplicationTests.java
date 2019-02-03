731b72689e8cc03375a4900187b5436225eddaaa
Stuti Sahoo <stsahoo2@in.ibm.com>
2019-02-03 19:30:11 +0530

    Changes to be committed: 	modified:   src/test/java/com/example/demo/DemoProjectApplicationTests.java 	modified:   target/test-classes/com/example/demo/DemoProjectApplicationTests.class

--- src/test/java/com/example/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/example/demo/DemoProjectApplicationTests.java
@@ -1,16 +1,30 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.controller.HelloController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoProjectApplicationTests {
 
+	
+	 @Autowired
+	  private HelloController controller;
+	 private TestRestTemplate restTemplate;
+	 
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
+	    assertThat(this.restTemplate.getForObject("http://localhost:" + 8080 + "/",
+	                String.class)).contains("Welcome back");
 	}
 
 }
